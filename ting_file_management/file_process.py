c4f430e33186afc00906a7182a7164da4f262a5a
uroque <ulissesroque@protonmail.com>
2023-01-11 22:42:08 -0300

    feat: implements process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    if content in instance._queue:
+        return None
+
+    instance.enqueue(content)
+    sys.stdout.write(str(content))
+    return content
 
 
 def remove(instance):
