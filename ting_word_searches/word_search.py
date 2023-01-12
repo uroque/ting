695175003ea9cc6e0aaddad73517cc3e2b4ab86a
uroque <ulissesroque@protonmail.com>
2023-01-12 15:37:28 -0300

    feat: implements exists_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for item in instance._queue:
+        occurrences = []
+
+        for index, line in enumerate(item["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1})
+
+        if len(occurrences) > 0:
+            result.append({
+                "palavra": word,
+                "arquivo": item['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
